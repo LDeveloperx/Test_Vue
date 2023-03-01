51499a7520f94fa8e1080b25a31910d7fdac88bb
dusan-lazic <dusan.ecole@gmail.com>
2023-03-01 17:51:38 +0100

    prviii

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarTogglerDemo03"
+        aria-controls="navbarTogglerDemo03"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="#">Test</a>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link link class="nav-link" to="/login">Login</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <router-view />
+  <br><br>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+  background-color: #43b6f8 !important;
+  a {
+    font-weight: 800;
+    color: #2c3e50;
+  }
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login">
+    <h1>Ulogujte se!</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm"></div>
+        <div class="col-sm">
+          <form>
+            <div class="form-group">
+              <h4 for="exampleInputEmail1">Email adresa</h4>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+            </div><br>
+            <div class="form-group">
+              <h4 for="exampleInputPassword1">Lozinka</h4>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+              />
+            </div>
+            <button type="button" class="btn btn-primary">Posalji</button>
+          </form>
+        </div>
+        <div class="col-sm"></div>
+      </div>
+    </div>
+  </div>
+</template>
